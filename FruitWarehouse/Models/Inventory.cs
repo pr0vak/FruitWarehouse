f316c7521870523cc563597bb9cf410178222d03
Vyacheslav <slava93omsk@mail.ru>
2025-03-16 10:12:42 +0600

    003 Добавление основных моделей (Инвентарь и Предмет).

---
+++ FruitWarehouse/Models/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace FruitWarehouse.Models
+{
+    public class Inventory
+    {
+        public Dictionary<(int, int), Item> Items { get; set; }
+
+
+
+        private Inventory() { }
+
+
+
+        public void AddItem(int x, int y, Item item)
+        {
+            Items[(x, y)] = item;
+        }
+
+        public void RemoveItem(int x, int y)
+        {
+            Items.Remove((x, y));
+        }
+
+
+
+        public static Inventory Create()
+        {
+            return new Inventory
+            {
+                Items = new Dictionary<(int, int), Item>()
+            };
+        }
+    }
+}
---
+++ FruitWarehouse/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace FruitWarehouse.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+
+
+
+        public Item(string name, string imagePath)
+        {
+            Name = name;
+            ImagePath = imagePath;
+        }
+    }
+}
